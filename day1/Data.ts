09f817a6be6c61ee546cd428486d7daec6d3dd70
Govardhan <2143906@cognizant.com>
2026-09-08 15:44:35 +0530

    Second Push

---
+++ day1/Data.ts
@@ -0,0 +1,29 @@
+var Myname = "Vardhan";
+console.log(Myname);
+var Vardhan = Myname.toUpperCase();
+console.log(Vardhan);
+var Vardhan1 = Myname.toLowerCase();
+console.log(Vardhan1);
+Myname.charAt(2);
+function fun() {
+    var add = 22;
+    var add1 = 23;
+    var add3 = add + add1;
+    console.log(Myname.charCodeAt(0));
+    console.log(Myname.concat(Vardhan1));
+    return add3;
+}
+console.log(fun);
+var login = true;
+var logout = false;
+console.log(login, logout);
+// Another way to declare the Variable
+var numb = 12345;
+console.log(numb);
+numb.toFixed();
+numb = 56;
+console.log(numb);
+var add = 22;
+var add1 = 23;
+console.log(Myname.charCodeAt(0));
+console.log(Myname.concat(Vardhan1));
